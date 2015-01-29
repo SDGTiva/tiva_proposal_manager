a6d3d9146ce8b58caedce9a0a7379a7402f246e0
Ângelo Nuffer <angelonuffer@gmail.com>
2015-01-29 22:37:22 +0000

    Move new proposals to proposals directory

--- install.sh
+++ install.sh
@@ -3,13 +3,20 @@
 create_directories()
 {
 	mkdir -p /usr/share/tiva/proposals/actions
-	chmod a+w /usr/share/tiva/proposals
+	mkdir -p /usr/share/tiva/proposals/news
+	chmod a+w /usr/share/tiva/proposals/news
 	mkdir -p /usr/share/tiva/acts
+}
+
+create_bins()
+{
+	cp sbin/tivapmd /usr/sbin/tivapmd
 }
 
 do_install()
 {
 	create_directories
+	create_bins
 }
 
 do_install
