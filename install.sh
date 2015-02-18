f369c1806d7b782a133c741d5ca3a1eab749ac56
Ângelo Nuffer <angelonuffer@gmail.com>
2015-02-18 11:55:51 +0000

    Turn actions directory writable and create tiva_manager group in installation script.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 create_directories()
 {
 	mkdir -p /usr/share/tiva/proposals/actions
+	chmod a+w /usr/share/tiva/proposals/actions
 	mkdir -p /usr/share/tiva/proposals/news
 	chmod a+w /usr/share/tiva/proposals/news
 	mkdir -p /usr/share/tiva/acts
@@ -13,10 +14,16 @@ create_bins()
 	cp sbin/tivapmd /usr/sbin/tivapmd
 }
 
+create_groups()
+{
+	addgroup tiva_manager
+}
+
 do_install()
 {
 	create_directories
 	create_bins
+	create_groups
 }
 
 do_install
