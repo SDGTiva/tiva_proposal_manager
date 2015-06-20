bb8d9f5edd752035424e8be08d265af9c45e78f6
Ângelo Nuffer <angelonuffer@gmail.com>
2015-06-20 03:03:29 +0000

    Use digital signatures instead of old file manipulation.

--- install.sh
+++ install.sh
@@ -2,21 +2,15 @@
 
 create_directories()
 {
-	mkdir -p /usr/share/tiva/proposals/actions
-	chmod a+w /usr/share/tiva/proposals/actions
-	mkdir -p /usr/share/tiva/proposals/news
-	chmod a+w /usr/share/tiva/proposals/news
+	mkdir -p /usr/share/tiva/proposals/pubkeys
+	mkdir -p /usr/share/tiva/proposals/signatures
+	chmod -R a+wt /usr/share/tiva/proposals
 	mkdir -p /usr/share/tiva/acts/log
 }
 
 create_bins()
 {
 	cp sbin/tivapmd /usr/sbin/tivapmd
-}
-
-create_groups()
-{
-	addgroup tiva_manager
 }
 
 create_scripts()
@@ -36,7 +30,6 @@ do_install()
 {
 	create_directories
 	create_bins
-	create_groups
 	create_scripts
 	create_webobjects
 }
