cd1c8815bdf543f1c2127350c4b5792fb0f82acd
Ângelo Nuffer <angelonuffer@gmail.com>
2015-06-23 22:09:20 +0000

    Do not remove all tiva directory on uninstall.

--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,8 @@
 #! /bin/sh
 
-remove_directories()
-{
-	rm -rf /usr/share/tiva
-}
-
 remove_bins()
 {
 	rm /usr/sbin/tivapmd
-}
-
-remove_groups()
-{
-	delgroup tiva_manager
 }
 
 remove_scripts()
@@ -23,9 +13,7 @@ remove_scripts()
 
 do_uninstall()
 {
-	remove_directories
 	remove_bins
-	remove_groups
 	remove_scripts
 }
 
