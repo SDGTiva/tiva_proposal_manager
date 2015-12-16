cacf823773fc3469687c5bf18cea24b0ad7a4447
Edilson Alzemand Sigmaringa Junior <edilson_alz@hotmail.com>
2015-12-16 11:00:53 -0200

    Update Install
    Bug fix on install

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #! /bin/sh
 
-create_directories()
-{
+create_directories(){
 	mkdir -p /usr/share/tiva/proposals/pubkeys
 	mkdir -p /usr/share/tiva/proposals/signatures
 	chmod -R a+wt /usr/share/tiva/proposals
 	mkdir -p /usr/share/tiva/acts/log
 }
 
-create_bins()
-{
+create_bins(){
 	cp sbin/tivapmd /usr/sbin/tivapmd
 }
 
-create_scripts()
-{
+create_scripts(){
 	cp init.d/tivapmd /etc/init.d/tivapmd
 	update-rc.d tivapmd defaults
 }
 
-do_install()
-{
+do_install(){
 	create_directories
 	create_bins
 	create_scripts
